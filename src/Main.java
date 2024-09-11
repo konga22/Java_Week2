eec305dae8db0bf01bc9ed3be48a42bce7469a58
konga22 <qwopop2@gmail.com>
2024-09-11 17:18:23 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 100;
+        boolean c = true;
+        char d = 'a';
+        String e = "eeeeasdasdassade";
+
+        System.out.println(a);//안녕하세여
+        System.out.println(b);//안녕하세여
+        System.out.println(e);//안녕하세여
+    }
+}
